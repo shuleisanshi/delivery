e2c45fd67048544eb16ef41e7337aba0dd818485
shulei <shuleisanshi@163.com>
2022-10-12 14:50:50 +0900

    Testing modify

--- reggie_take_out/src/main/java/com/itheima/reggie/controller/UserController.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/controller/UserController.java
@@ -69,7 +69,9 @@ public class UserController {
         String code = map.get("code").toString();
 
         //从Session中获取保存的验证码
-        Object codeInSession = session.getAttribute(phone);
+        //Object codeInSession = session.getAttribute(phone);
+
+        String codeInSession = "1234";
 
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
         if(codeInSession != null && codeInSession.equals(code)){
